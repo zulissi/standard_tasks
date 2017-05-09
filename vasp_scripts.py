e05dbf0b352f1e4a79367644d28cb9080dec00dd
Zachary Ulissi <zulissi@gmail.com>
2017-05-09 15:27:40 -0400

    update for configs for arjuna/gilgamesh/cori

--- vasp_scripts.py
+++ vasp_scripts.py
@@ -15,15 +15,41 @@ def runVasp(fname_in,fname_out,vaspflags,npar=4):
     Vasp.vasprc(mode='run')
     #set ppn>1 so that it knows to do an mpi job, the actual ppn will guessed by Vasp module
     Vasp.VASPRC['queue.ppn']=2
-    if vaspflags['xc']=='beef-vdw':
+    #if vaspflags['xc']=='beef-vdw':
         #if we're doing PBE, we're probably on a KNL node
-        vaspflags['NCORE']=1
-    else:
-        NNODES=int(os.environ['SLURM_NNODES'])
-        vaspflags['KPAR']=NNODES
+        #vaspflags['NCORE']=1
+    #else:
+    #    NNODES=int(os.environ['SLURM_NNODES'])
+    #    vaspflags['KPAR']=NNODES
+
+    if 'PBS_SERVER' in os.environ and os.environ['PBS_SERVER']=='gilgamesh.cheme.cmu.edu':
+        #We're on gilgamesh
+        vaspflags['NPAR']=4
+    elif 'SLURM_CLUSTER_NAME' in os.environ and os.environ['SLURM_CLUSTER_NAME']=='arjuna':
+        #We're on arjuna
+        if os.environ['CUDA_VISIBLE_DEVICES']!='NoDevFiles':
+            #We have a GPU job on arjuna
+            vaspflags['NCORE']=1
+            vaspflags['KPAR']=16
+            vaspflags['NSIM']=8
+        else:
+            #We're running CPU only
+            vaspflags['NCORE']=4
+            vaspflags['KPAR']=2
+    elif 'SLURM_CLUSTER_NAME' in os.environ and	os.environ['SLURM_CLUSTER_NAME']=='cori':
+        #We're on cori
+        if os.environ['CRAY_CPU_TARGET']=='haswell':
+            #We're on a haswell CPU node
+            NNODES=int(os.environ['SLURM_NNODES'])
+            vaspflags['KPAR']=NNODES
+        elif os.environ['CRAY_CPU_TARGET']=='knl':
+            vaspflags['NCORE']=1
+
+    pseudopotential=vaspflags['pp_version']
+    os.environ['VASP_PP_PATH']=os.environ['VASP_PP_BASE']+'/'+str(pseudopotential)+'/'
+
     #set up the calculation and run
     calc=Vasp('./',atoms=atoms,**vaspflags)
-    calc.update()
     calc.read_results()
         
     #Get the final trajectory
